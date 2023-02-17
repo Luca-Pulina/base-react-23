6a45c89ecb9baeb4ef402ee9196bbf763b1f8dd8
Luca-Pulina <luca.pulina@gmail.com>
2023-02-17 21:04:21 +0100

    feat: better configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{html,ts,tsx}', './src/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        border: '0px 1px 6px 7px rgba(0,0,0,0.38)',
+      },
+    },
   },
   plugins: [],
 }
